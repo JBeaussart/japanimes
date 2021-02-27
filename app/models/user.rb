d319de6288488b59b165bf3a2992c19c8c449925
jeremy beaussart <jeremy.beaussart@gmail.com>
2021-02-27 15:59:03 +0100

    rename all file model watchlists to watchlist_animes

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
 	has_many :reviews, dependent: :destroy
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  has_many :watchlists, dependent: :destroy
+  has_many :watchlist_animes, dependent: :destroy
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
