c4ec033c1a8a992ff4f3315aeb6d298af6b3fef5
Thomas Pozzobon <thomaspozzobon86@gmail.com>
2021-02-27 14:31:45 +0100

    Create model watchlist

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_27_131318) do
+ActiveRecord::Schema.define(version: 2021_02_27_133054) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,4 +28,12 @@ ActiveRecord::Schema.define(version: 2021_02_27_131318) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  create_table "watchlists", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_watchlists_on_user_id"
+  end
+
+  add_foreign_key "watchlists", "users"
 end
