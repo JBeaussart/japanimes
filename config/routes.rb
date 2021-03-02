ed8b0105a62f20a33ef5da265ec2f720840fe541
Thomas Pozzobon <thomaspozzobon86@gmail.com>
2021-03-02 19:47:02 +0100

    add users resources

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
 
+  resources :users, only: :show
   resources :animes, only: [ :index, :show ] do
     resources :reviews, only: :create
     resources :watchlist_animes, only: :create
   end
-  resources :watchlist_animes, only: :destroy 
+  resources :watchlist_animes, only: :destroy
   resources :reviews, only: [ :update, :destroy ]
 end
