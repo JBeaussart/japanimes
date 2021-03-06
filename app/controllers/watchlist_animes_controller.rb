a42c849179a99fb560184a84a087d9951761dc83
Quentin Bonnafet <quentin.bonnafet@gmail.com>
2021-03-06 11:04:53 +0100

    save watchlist controller

--- app/controllers/watchlist_animes_controller.rb
+++ app/controllers/watchlist_animes_controller.rb
@@ -1,12 +1,21 @@
 class WatchlistAnimesController < ApplicationController
   def create
     @anime = Anime.find(params[:anime_id])
-    @watchlist_animes = WatchlistAnime.new(user_id: current_user.id, anime_id: @anime.id)
-    if @watchlist_animes.save!
-      redirect_to user_path(current_user)
+    if current_user.nil?
+      redirect_to anime_path(@anime), notice: "Vous devez vous connecter"
     else
-      render "animes/show"
+      @watchlist_animes = WatchlistAnime.new(user_id: current_user.id, anime_id: @anime.id)
+      if @watchlist_animes.save
+        redirect_to user_path(current_user), notice: "Votre Watchlist a été mise à jour"
+      else
+        render "animes/show"
+      end
     end
   end
 
+  def destroy
+    @watchlist_animes = WatchlistAnime.find(params[:id])
+    @watchlist_animes.destroy
+    redirect_to user_path(current_user)
+  end
 end
