0887bebc11d901760280736a89246392c361d758
jeremy beaussart <jeremy.beaussart@gmail.com>
2021-03-09 19:00:12 +0100

    add heart animation to turbolink

--- app/javascript/custom/heart_animation.js
+++ app/javascript/custom/heart_animation.js
@@ -1,22 +1,22 @@
-const like = document.querySelectorAll('.like');
-
-
-like.forEach((element) => {
-    let countLike = 0;
-    element.addEventListener('click', () => {
-    
-        if(countLike === 0) {
+export const heart_animation = () => {
+    const like = document.querySelectorAll('.like');
+    like.forEach((element) => {
+        let countLike = 0;
+        element.addEventListener('click', () => {
+        
+            if(countLike === 0) {
+                element.classList.toggle('anim-like');
+                countLike = 1;
+                element.style.backgroundPosition = 'right';
+            } else {
+                countLike = 0;
+                element.style.backgroundPosition = 'left';
+            }
+        
+        });
+        
+        element.addEventListener('animationend', () => {
             element.classList.toggle('anim-like');
-            countLike = 1;
-            element.style.backgroundPosition = 'right';
-        } else {
-            countLike = 0;
-            element.style.backgroundPosition = 'left';
-        }
-    
-    });
-    
-    element.addEventListener('animationend', () => {
-        element.classList.toggle('anim-like');
+        })
     })
-})
+};
