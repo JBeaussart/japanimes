01e47dd10a7e990006b9dd32289793417962d04f
Thomas Pozzobon <thomaspozzobon86@gmail.com>
2021-03-09 20:27:56 +0100

    change color of site's theme

--- app/javascript/custom/toggle_menu.js
+++ app/javascript/custom/toggle_menu.js
@@ -2,7 +2,7 @@ export const listenToNavbarToggle = () => {
   const menuToggle     = document.querySelector(".toggle");
   const menuToggleIcon = document.querySelector(".toggle > i.fas");
   const menu           = document.querySelector(".menu");
-  const title          = document.querySelector(".title");
+  const logo          = document.querySelector(".logo-navbar");
   const overlay        = document.querySelector(".overlay");
   
   menuToggle.addEventListener('click', () => {
@@ -10,7 +10,7 @@ export const listenToNavbarToggle = () => {
     menuToggleIcon.classList.toggle("fa-bars");
     menuToggleIcon.classList.toggle("fa-times");
     menu.classList.toggle("active");
-    title.classList.toggle("active");
+    logo.classList.toggle("active");
     overlay.classList.toggle("has-overlay");
   });
 }
