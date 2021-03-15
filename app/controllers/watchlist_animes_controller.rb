fdade752d5f28ee9fe99bb8812ad2137ef50b101
jeremy beaussart <jeremy.beaussart@gmail.com>
2021-03-15 21:02:08 +0100

    delete solve need to iterate inside controller watchlist

--- app/controllers/watchlist_animes_controller.rb
+++ app/controllers/watchlist_animes_controller.rb
@@ -19,7 +19,10 @@ class WatchlistAnimesController < ApplicationController
   end
 
   def destroy
-    @watchlist_animes = WatchlistAnime.find(params[:id])
+    @watchlist_ids = current_user.watchlist_animes.ids
+    @watchlist_ids.each do |watchlist_id|
+      @watchlist_animes = WatchlistAnime.find(watchlist_id)
+    end
     @watchlist_animes.destroy
     redirect_to animes_path, notice: "Anime supprimé de votre Watchlist"
   end
