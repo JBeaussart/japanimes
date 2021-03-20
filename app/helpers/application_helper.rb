b82d652e582813221f0b48ee43870a278164d4c9
Quentin Bonnafet <quentin.bonnafet@gmail.com>
2021-03-20 10:29:14 +0100

    logo in show anime

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+  def logo_streaming_service(streaming_service)
+    case streaming_service 
+    when "Wakanim"
+      image_tag('wakanim_logo.png', class: "wakanim-logo")
+    when "Netflix"
+      image_tag('netflix_logo.png', class: "netflix-logo")
+    when "Crunchyroll"
+      image_tag('crunchyroll_logo.png', class: "crunchyroll-logo")
+    when "ADN"
+      image_tag('adn_logo.png', class: "adn-logo")
+    end
+  end
 end
