3e30182c20af3842f28441f12e88c81f9e4678cc
jeremy beaussart <jeremy.beaussart@gmail.com>
2021-03-21 15:41:31 +0100

    replace jQuery synthax to Vanilla JS and when counter ++ diplay grand maitre

--- app/javascript/custom/upvote_card.js
+++ app/javascript/custom/upvote_card.js
@@ -1,14 +1,16 @@
 export const upvote = () => {
- 
-  var counter = 99;
-  
-  $("#plus").click(function(){
+
+  let counter = 99;
+  const grandMaitre     = document.querySelector(".grand-maitre");
+
+  document.querySelector("#plus").addEventListener('click', () => {
     counter++;
-    $("#count").text(counter);
+    document.querySelector("#count").textContent = (counter);
+    grandMaitre.classList.toggle("active-show");
   });
-  
-  $("#minus").click(function(){
-    counter--;    
-    $("#count").text(counter);
+
+  document.querySelector("#minus").addEventListener('click', () => {
+    counter--;
+    document.querySelector("#count").textContent = (counter);
   });
 }
