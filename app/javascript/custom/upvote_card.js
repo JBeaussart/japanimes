0f65f63c4b215de9cbe038fc68679ab188783018
jeremy beaussart <jeremy.beaussart@gmail.com>
2021-03-21 15:58:00 +0100

    remove initie and add maitre when click on plus

--- app/javascript/custom/upvote_card.js
+++ app/javascript/custom/upvote_card.js
@@ -2,10 +2,12 @@ export const upvote = () => {
 
   let counter = 99;
   const grandMaitre     = document.querySelector(".grand-maitre");
+  const initie = document.querySelector(".initie")
 
   document.querySelector("#plus").addEventListener('click', () => {
     counter++;
     document.querySelector("#count").textContent = (counter);
+    initie.classList.toggle("inite-show");
     grandMaitre.classList.toggle("active-show");
   });
 
