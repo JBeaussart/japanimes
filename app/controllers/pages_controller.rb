a71e76c6f12a40a3a7ed01c7bd5ee2332bccbd42
Thomas Pozzobon <thomaspozzobon86@gmail.com>
2021-03-23 20:30:25 +0100

    connect streaming service button to link

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,7 +3,11 @@ class PagesController < ApplicationController
 
   def home
     @animes = Anime.all
+
     unfiltered_categories = @animes.map { |anime| anime.category } 
     @categories = unfiltered_categories.uniq
+
+    unfiltered_streaming_services = @animes.map { |anime| anime.streaming_service } 
+    @streaming_services = unfiltered_streaming_services.uniq
   end
 end
