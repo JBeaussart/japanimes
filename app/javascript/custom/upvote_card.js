4a1b563335ad0ea1dfd92e13b28e869ed040ae4a
jeremy beaussart <jeremy.beaussart@gmail.com>
2021-03-23 19:58:34 +0100

    add animation to appear maitre

--- app/javascript/custom/upvote_card.js
+++ app/javascript/custom/upvote_card.js
@@ -1,14 +1,17 @@
 export const upvote = () => {
 
   let counter = 99;
-  const grandMaitre     = document.querySelector(".grand-maitre");
-  const initie = document.querySelector(".initie")
+  // const grandMaitre = document.querySelector(".grand-maitre");
+  // const initie      = document.querySelector(".initie")
+  const rank      = document.querySelector("#rank_theory")
 
   document.querySelector("#plus").addEventListener('click', () => {
     counter++;
     document.querySelector("#count").textContent = (counter);
-    initie.classList.toggle("inite-show");
-    grandMaitre.classList.toggle("active-show");
+    // initie.classList.toggle("inite-show");
+    // grandMaitre.classList.toggle("active-show");
+    rank.textContent = "";
+    rank.classList.add("rank");
   });
 
   document.querySelector("#minus").addEventListener('click', () => {
